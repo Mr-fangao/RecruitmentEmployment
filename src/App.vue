f84e5b4335db2a954015353494196d2729bd5001
Mr-fangao <Mr.undefine@protonmail.com>
2021-10-21 19:27:49 +0800

    1
    1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
+  <div id="app">
+    <Header v-show="$route.name!=='login'"></Header>
+<!--    <img src="./assets/logo.png">-->
     <router-view/>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+  }
 }
 </script>
 
 <style>
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
 <template>
-
+  <div class="haed">
+    <el-button style="width: 150px;margin-left: 15px">后台管理</el-button>
+    <el-button style="width: 150px;background-color: aqua">22</el-button>
+    <el-button style="width: 150px">33</el-button>
+    <el-button style="width: 500px">99</el-button>
+    <el-button style="width: 150px">44</el-button>
+    <el-button style="width: 150px">55</el-button>
+    <el-button style="width: 150px;margin-right: 15px">66</el-button>
+  </div>
 </template>
 
 <script>
+// import '../../assets/css/global.css'
 export default {
-  name: "Header"
+  name: "Header",
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.head{
+  height: 60px;
+  line-height: 60px;
+  display: flex;
+  background-color: aqua;
+}
 
 </style>
