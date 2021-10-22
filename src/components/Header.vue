6c723e3480e0c733d9c7e5f77cbefb16ffaf7048
gxzan <75480599+gxzan@users.noreply.github.com>
2021-10-22 16:27:40 +0800

    10221627

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,38 @@
 <template>
-  <el-header class="d-flex align-items-center" style="background: transparent">
-    <el-menu
-      :default-active="navBarIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="transparent"
-      active-text-color="#4edeff"
+  <div class="header">
+    <el-header
+      class="d-flex align-items-center"
+      style="background: transparent"
     >
-      <el-menu-item index="1">数据面板</el-menu-item>
-      <el-menu-item index="1">职位搜索</el-menu-item>
-      <el-menu-item index="1">空间查询</el-menu-item>
-      <el-menu-item index="1">技能需求</el-menu-item>
-    </el-menu>
-    <h1>行业招聘大数据分析与就业服务系统</h1>
-    <el-menu
-      :default-active="navBarIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="transparent"
-      text-color="#fff"
-      active-text-color="#4edeff"
-    >
-      <el-menu-item index="1">专题地图</el-menu-item>
-      <el-menu-item index="1">时空分析</el-menu-item>
-      <el-menu-item index="1">就业推荐</el-menu-item>
-      <el-submenu index="100">
-        <template slot="title">
-          <!-- 头像 -->
-          <el-avatar
-            :size="small"
-            src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
-          ></el-avatar>
-          silas
-        </template>
-        <el-menu-item index="100-1">修改</el-menu-item>
-        <el-menu-item index="100-2">退出</el-menu-item>
-      </el-submenu>
-    </el-menu>
-  </el-header>
+      <el-menu
+        :default-active="navBarIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="transparent"
+        active-text-color="#4edeff"
+      >
+        <el-menu-item index="1">数据面板</el-menu-item>
+        <el-menu-item index="1">职位搜索</el-menu-item>
+        <el-menu-item index="1">空间查询</el-menu-item>
+        <el-menu-item index="1">技能需求</el-menu-item>
+      </el-menu>
+      <h1>行业招聘大数据分析与就业服务系统</h1>
+      <el-menu
+        :default-active="navBarIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="transparent"
+        text-color="#fff"
+        active-text-color="#4edeff"
+      >
+        <el-menu-item index="1">专题地图</el-menu-item>
+        <el-menu-item index="1">时空分析</el-menu-item>
+        <el-menu-item index="1">就业推荐</el-menu-item>
+      </el-menu>
+    </el-header>
+  </div>
 </template>
 
 <script>
@@ -60,12 +53,23 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
-.el-header {
-  display: flex;
-  flex-direction: row;
-  .el-menu-item {
-    font-size: 20px;
+.header {
+  height: 60px;
+  width: 100%;
+  background: url(../assets/img/headerbg.png);
+  background-size: 100% 100%;
+  .el-header {
+    display: flex;
+    flex-direction: row;
+    .el-menu-item {
+      font-size: 20px;
+    }
+  }
+  h1 {
+    font-size: 24px;
+    color: #ffffff;
+    font-family: Microsoft YaHei;
+    font-weight: bold;
   }
 }
 
