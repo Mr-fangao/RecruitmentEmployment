732767100920dea809c0f526534cbd7d1a3ddf40
chenjq001 <2862053319@qq.com>
2021-11-20 20:49:44 +0800

    更新 factorstatistics.vue

--- src/views/thematicmap/factorstatistics.vue
+++ src/views/thematicmap/factorstatistics.vue
@@ -1,7 +1,7 @@
 <!--多要素统计-->
 <template>
   <div>
-  <iframe  src="mapbox://styles/chenjq/ckw7ey2ns5vb314rzc87f3o1f" width="100%" height="100%" frameborder="0"></iframe>
+  <iframe  src="src\assets\html\GraphCType.html" width="100%" height="100%" frameborder="0"></iframe>
   </div>
 </template>
 
