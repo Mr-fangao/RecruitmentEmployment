2bb0981561a58e25aa3ad150b2ebe89cacdca86a
Mr-fangao <Mr.undefine@protonmail.com>
2021-11-21 12:45:36 +0800

    1

--- src/views/educationstatistics.vue
+++ src/views/educationstatistics.vue
@@ -1,42 +1,145 @@
 <template>
   <div class="grid-container">
-    <div class="item1">1</div>
+    <div class="item1">
+      <div class="content">
+        <div class="title">
+          <dir class="select">
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                Dropdown List
+                <el-icon class="el-icon--right">
+                  <arrow-down />
+                </el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>Action 1</el-dropdown-item>
+                  <el-dropdown-item>Action 2</el-dropdown-item>
+                  <el-dropdown-item>Action 3</el-dropdown-item>
+                  <el-dropdown-item>Action 4</el-dropdown-item>
+                  <el-dropdown-item>Action 5</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </dir>
+        </div>
+        <div class="chart">
+          <div id="chart1" :style="{ width: '100%', height: '100%' }"></div>
+        </div>
+      </div>
+    </div>
     <div class="item2">2</div>
     <div class="item3">3</div>
-    <div class="item4">
-      <div id="map"></div>
-    </div>
-    <div class="item5">5</div>
-    <div class="item6">6</div>
-    <div class="item7">7</div>
+    <div class="item4">4</div>
   </div>
 </template>
 
 <script>
+let echarts = require("echarts/lib/echarts");
+require("echarts/lib/chart/bar");
+require("echarts/lib/chart/line");
+require("echarts/lib/chart/pie");
+require("echarts/lib/chart/scatter");
+require("echarts/lib/chart/funnel");
+require("echarts/lib/component/tooltip");
+require("echarts/lib/component/title");
+require("echarts/lib/component/legend");
 export default {
   name: "education",
-  data() {},}
+
+  data() {},
+  mounted() {
+    this.chart1();
+  },
+  methods: {
+    chart1() {
+      // 基于准备好的dom，初始化echarts实例
+      let Chart1 = echarts.init(document.getElementById("chart1"));
+      // 绘制图表
+      var option;
+      option = {
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          orient: "vertical",
+          left: "5%", //图例距离左的距离
+          y: "center", //图例上下居中
+          data: ["A", "B", "C", "D", "E"],
+          textStyle: {
+            //图例文字的样式
+            color: "#fff",
+            fontSize: 12,
+          },
+        },
+
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            center: ["60%", "55%"],
+            radius: ["70%", "50%"],
+            label: {
+              normal: {
+                show: true,
+                position: "outside",
+                formatter: "{b}",
+              },
+            },
+            labelLine: {
+              normal: {
+                show: true,
+                length: 50,
+                length2: 10,
+              },
+            },
+            data: [
+              { value: 1048, name: "A" },
+              { value: 735, name: "B" },
+              { value: 580, name: "C" },
+              { value: 484, name: "D" },
+              { value: 300, name: "E" },
+            ],
+          },
+        ],
+      };
+      option && Chart1.setOption(option);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .grid-container {
   display: grid;
   height: calc(100% - 45px);
-  grid-template-columns: 1fr 3fr 1fr;
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(2, 1fr);
   grid-template-areas:
-    "pt1 pt4 pt5 "
-    "pt2 pt4 pt6 "
-    "pt3 pt4 pt7 ";
+    "pt1 pt3  "
+    "pt2 pt4  ";
   background-color: transparent;
+  // .title{
 
+  // }
+
+  .chart {
+    height: 350px;
+  }
   div {
     text-align: center;
     font-size: 30px;
-    //outline: 1px dashed rgb(136, 27, 27);
+    outline: 1px dashed rgb(136, 27, 27);
   }
   .item1 {
     grid-area: pt1;
+    .title {
+      float: top;
+      height: 70px;
+      .select {
+
+      }
+    }
   }
   .item2 {
     grid-area: pt2;
@@ -46,15 +149,6 @@ export default {
   }
   .item4 {
     grid-area: pt4;
-  }
-  .item5 {
-    grid-area: pt5;
-  }
-  .item6 {
-    grid-area: pt6;
-  }
-  .item7 {
-    grid-area: pt7;
   }
 }
 </style>
