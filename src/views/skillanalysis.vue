6265b1d64047083f8771d4d4af2f4c1fd4b81b1b
chenjq001 <2862053319@qq.com>
2021-12-10 22:38:40 +0800

    地图组件

--- src/views/skillanalysis.vue
+++ src/views/skillanalysis.vue
@@ -1,13 +1,22 @@
 <template>
-
+  <div class="mainpage">
+    <mapbox-test/>
+  </div>
 </template>
 
 <script>
+import mapboxtest from "../components/map/maptest.vue";
 export default {
-  name: "analysis"
-}
+  name: "analysis",
+  components: {
+    mapboxtest,
+  },
+};
 </script>
 
 <style scoped>
-
+  .mainpage{
+    width: 100%;
+    height: 100%;
+  }
 </style>
