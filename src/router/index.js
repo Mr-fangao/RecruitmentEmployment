74995c4a059343a5dab78672663f82be92905f67
Mr-fangao <Mr.undefine@protonmail.com>
2021-12-10 13:39:07 +0800

    打包部署问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
@@ -92,7 +93,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // mode:Hash,
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
