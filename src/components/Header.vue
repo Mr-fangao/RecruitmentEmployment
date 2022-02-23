0c2252eaf417db7d82fc549e0cd780b45ae2882c
gxzan <2465988596@qq.com>
2022-02-23 21:14:27 +0800

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/recommend" exact>智能推荐</router-link>
-        <router-link to="/query" exact>职位查询</router-link>
-        <router-link to="/data" exact>就业统计</router-link>
-        <h1>前程无忧 · 就业推荐与智能分析系统</h1>
-        <router-link to="/skAnalysis" exact>综合分析</router-link>
-        <router-link to="/skill" exact>技能分析</router-link>
-        <router-link to="/thememap" exact>专题地图</router-link>
-      </li>
-    </ul>
-    <span class="times">{{ time }}</span>
-  </nav>
+    <nav>
+      <ul>
+        <li>
+          <h1>就业推荐与智能分析系统</h1>
+          <router-link to="/query" exact>职位查询</router-link>
+          <router-link to="/data" exact>就业统计</router-link>
+          <router-link to="/skAnalysis" exact>综合分析</router-link>
+          <router-link to="/skill" exact>技能分析</router-link>
+          <router-link to="/thememap" exact>专题地图</router-link>
+        </li>
+      </ul>
+      <img src="../assets/img/time.png" />
+      <span class="times">{{ time }}</span>
+    </nav>
 </template>
 
 <script>
@@ -41,8 +41,7 @@ export default {
         new Date().getSeconds() < 10
           ? "0" + new Date().getSeconds()
           : new Date().getSeconds();
-      this.time =
-        mm + "月" + dd + "日 " + hh + ":" + mf + ":" + ss;
+      this.time = mm + "月" + dd + "日 " + hh + ":" + mf + ":" + ss;
     },
   },
 };
@@ -54,16 +53,13 @@ nav {
   flex-direction: row;
   align-items: center;
   height: 50px;
-  background: url(../assets/img/headerbg.png);
-  background-size: 100% 100%;
   padding: 0px;
   .times {
-    width: 50px;
-    margin:0px 5px 0px 30px;
-    font-size: 10px;
+    width: 80px;
+    margin: 0px 5px 0px 30px;
+    font-size: 16px;
     float: right;
     font-family: Microsoft YaHei;
-    
     color: #ffffff;
   }
   ul {
@@ -71,38 +67,40 @@ nav {
     list-style-type: none; /*消除黑点*/
     text-align: center;
     margin: 0px;
+    background: url(../assets/img/header_bg.png);
+    background-size: 100% 100%;
     li {
       height: 100%;
       display: flex;
       flex-direction: row;
       align-items: center;
-      margin: 0px 0px 0px 40px;
-      
+      margin: 0px;
       h1 {
         font-size: 26px;
-        color: #ffffff;
+        color: #1bcddd;
         font-family: Microsoft YaHei;
-      
-        margin:0px 105px 10px 160px;
+        margin: 0px 20px 0px 20px;
       }
       a {
         font-size: 18px;
+        width: 90px;
         color: #fff; /*设置字体颜色*/
         font-family: Microsoft YaHei;
         text-decoration: none; /*消除下划线*/
         border-radius: 5px;
         margin: 0px 0px 0px 45px;
-        padding:0px 2px 2px 2px;
+        padding: 0px 2px 2px 2px;
+        line-height: 32px;
       }
     }
   }
 }
 /*选中则发生一些变化*/
+
 .router-link-active {
   cursor: pointer;
-
-  color: #ffffff;
+  color: #1bcddd;
   background: url(../assets/img/menucurrent.png);
-  background-position: 49% 25%;
+  background-size: 100% 100%;
 }
 </style>
