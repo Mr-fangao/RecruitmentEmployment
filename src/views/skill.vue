5ff5c4626e4e459875b3dee30b7d5750e3c351cb
gxzan <2465988596@qq.com>
2022-02-25 21:45:08 +0800

    地图

--- src/views/skill.vue
+++ src/views/skill.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="skill">
+    <div id="map"></div>
     <div class="left">
       <div class="pt 1"></div>
       <div class="relation">
@@ -10,13 +11,18 @@
       </div>
     </div>
     <div class="right">
-      <div id="map"></div>
-      <div class="pt 4">
-        <div class="hd">
-          <span>123</span>
+      <div class="part">
+        <div class="pt 4">
+          <div class="hd">
+            <span>123</span>
+          </div>
+        </div>
+        <div class="pt 5">
+          <div class="hd">
+            <span>123</span>
+          </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -27,18 +33,44 @@ export default {
   data() {
     return {};
   },
+  mounted() {
+    this.initmap();
+  },
+  methods: {
+    initmap() {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiY2hlbmpxIiwiYSI6ImNrcWFmdWt2bjBtZGsybmxjb29oYmRzZzEifQ.mnpiwx7_cBEyi8YiJiMRZg";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox:/ylesenjq/cl010ychv001214pdpa5xyq5a",
+        center: [105, 35],
+        zoom: 3.5,
+      });
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
-.query {
+#map {
+  position: relative;
+  z-index: 0;
+    margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+.skill {
+  position: fixed;
   display: flex;
   height: calc(100% - 50px);
   background-color: transparent;
   .left {
+    position: absolute;z-index: 999;
+    top: 1;
     display: flex;
     height: 100%;
-    width: 78%;
+    width: 40%;
     flex-direction: column;
     .pt1 {
       width: 100%;
@@ -50,11 +82,22 @@ export default {
     }
   }
   .right {
+    position: absolute;
+    z-index: 999;
     display: flex;
+    top: 0%;
     height: 100%;
-    width: 22%;
+    width: 60%;
     flex-direction: column;
     justify-items: center;
+    .part {
+      height: 30%;
+      width: 100%;
+      .pt4 {
+        width: 50%;
+        height: 100%;
+      }
+    }
   }
   .pt {
     height: 215px;
