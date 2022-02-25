3666979ebb8b35cbe3d0eb4374d886a52274d8ed
gxzan <2465988596@qq.com>
2022-02-25 15:59:25 +0800

    表格样式

--- src/views/query.vue
+++ src/views/query.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="query">
     <div class="left">
-      <div class="pt 1">
+      <div class="main"><div id="map"></div></div>
+      <div class="bottom">
+        <div class="">
         <div class="hd">
           <span>123</span>
         </div>
         <div class="ct"></div>
       </div>
-      <div class="pt 2"></div>
-      <div class="pt 3"></div>
-    </div>
-    <div class="middle">
-      <div id="map"></div>
     </div>
     <div class="right">
       <div class="pt 4">
@@ -43,12 +40,18 @@ export default {
   .left {
     display: flex;
     height: 100%;
-    width: 22%;
+    width: 78%;
     flex-direction: column;
-  }
-  .middle {
-    height: 100%;
-    width: 66%;
+    .main {
+      width: 100%;
+      height: 60%;
+    }
+    .pt1 {
+      margin-bottom: 15px;
+      width: 100%;
+      background: url(../assets/img/pt/pt_bg.png) no-repeat;
+      background-size: 100% 100%;
+    }
   }
   .right {
     display: flex;
